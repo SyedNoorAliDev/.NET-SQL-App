f9ff9d909c2d79cd86f1f9fbcee709d6ee1cf3de
Syed Noor Ali <96229280+SyedNoorAliDev@users.noreply.github.com>
2023-09-26 12:20:22 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.ReadOnly = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Form1_Load() called...");
+            textBox3.Text = "Startup..."; try
+            {
+                System.Diagnostics.Debug.WriteLine("within the try");
+                SqlConnection connection = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                connection.Open();
+                textBox3.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                textBox3.Text = "Error, " + ex;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            textBox3.Text = "Retrieving Records...";
+            command.Connection = connection;
+            command.CommandText = "select * from Customers";
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            textBox3.Text = "Retrieval Successful!";
+            connection.Close();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            dataGridView1.Text = "Counting Records...";
+            command.Connection = connection;
+            command.CommandText = "select count(*) from Customers";
+            int count = (int)command.ExecuteScalar();
+            textBox3.Text = "Number of records: " + count;
+            connection.Close();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.ReadOnly = true;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            textBox3.Text = "Inserting Record...";
+            command.Connection = connection;
+            command.CommandText = "insert into Customers (CustomerID, CompanyName) values('" +textBox1.Text + "','" + textBox2.Text + "')";
+            command.ExecuteNonQuery();
+            textBox3.Text = "Record Inserted...";
+            connection.Close();
+        }
+    }
+}
